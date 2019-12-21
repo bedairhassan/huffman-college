9dbaf54f8ad1e9a64b146d560f9b13c09bd15a99
hankerhankk11 <55901623+hankerhankk11@users.noreply.github.com>
2019-12-21 15:32:15 +0200

    here

--- playground/pg2/first.js
+++ playground/pg2/first.js
@@ -1,27 +1,26 @@
 var promise1 = new Promise(function(resolve, reject) {
-    setTimeout(function() {
-
-        let returnnthis = 2
+    let returnnthis = 2
 
             // let returnnthis={
             //     'id':'1',
             //     'description':2
             // }
 
-      resolve(returnnthis);
-    }, 300);
+      resolve(returnnthis)
   });
   
   promise1.then(function(value) {
-    console.log(value);
     return value*2
     // expected output: "foo"
   }).then(function (value){
 
-    console.log(value*2)
+    return value*2
+  }).then(function(value){
+
+    console.log(value)
   })
   
-  console.log(promise1);
+  
   // expected output: [object Promise]
   
   
